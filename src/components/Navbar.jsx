de048369896216f18f9a70aff1f151315c65a4fa
aaroncutkelvim <aaroncutkelvin@gmail.com>
2022-03-09 23:59:45 -0600

    add asset update2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
+import profile_pic from "./images/profile pic.PNG";
 
 const pages = ["Home", "Add_Asset", "All_AssetS", "Branch_Assets", "Update"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -109,7 +110,7 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src={profile_pic} />
               </IconButton>
             </Tooltip>
             <Menu
