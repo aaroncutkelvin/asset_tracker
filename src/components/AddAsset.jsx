2977adf86e99a56de2da2e5cb31db50b4f2c7a05
aaroncutkelvim <aaroncutkelvin@gmail.com>
2022-03-09 23:46:59 -0600

    add asset update1

--- src/components/AddAsset.jsx
+++ src/components/AddAsset.jsx
@@ -8,12 +8,13 @@ import InputAdornment from "@mui/material/InputAdornment";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import TimePicker from "@mui/lab/TimePicker";
-import DateTimePicker from "@mui/lab/DateTimePicker";
+import MenuItem from "@mui/material/MenuItem";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
-import MobileDatePicker from "@mui/lab/MobileDatePicker";
+import Button from "@mui/material/Button";
 
 function AddAsset() {
-  const [value, setValue] = React.useState("Controlled");
+  const [value, setValue] = React.useState("");
+
   const [values, setValues] = React.useState({
     amount: "",
     password: "",
@@ -21,6 +22,45 @@ function AddAsset() {
     weightRange: "",
     showPassword: false,
   });
+
+  const branches = [
+    {
+      value: "BZE",
+      label: "Belize City",
+    },
+    {
+      value: "LVIL",
+      label: "Ladyville",
+    },
+    {
+      value: "Bmp",
+      label: "Belmopan",
+    },
+    {
+      value: "SEL",
+      label: "Santa Elena",
+    },
+    {
+      value: "SIG",
+      label: "San Ignacio",
+    },
+    {
+      value: "OW",
+      label: "Orange Walk",
+    },
+    {
+      value: "CZL",
+      label: "Corozal",
+    },
+    {
+      value: "DNG",
+      label: "Dangriga",
+    },
+    {
+      value: "PG",
+      label: "Punta Gorda",
+    },
+  ];
 
   const handleChanges = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
@@ -30,10 +70,22 @@ function AddAsset() {
     setValue(event.target.value);
   };
 
-  const [val, setVal] = React.useState(new Date("2022-03-10T21:11:54"));
+  const [val, setVal] = React.useState(new Date("2022-03-10T00:00:00"));
 
   const handleFix = (newVal) => {
     setVal(newVal);
+  };
+
+  const [time, setTime] = React.useState(new Date("2022-09-10T06:30:00"));
+
+  const handleTime = (newTime) => {
+    setTime(newTime);
+  };
+
+  const [branch, setBranch] = React.useState("EUR");
+
+  const handleBranch = (event) => {
+    setBranch(event.target.value);
   };
 
   return (
@@ -77,6 +129,13 @@ function AddAsset() {
             onChange={handleFix}
             renderInput={(params) => <TextField {...params} />}
           />
+          <DesktopDatePicker
+            label="Expiration Date"
+            inputFormat="MM/dd/yyyy"
+            value={time}
+            onChange={handleTime}
+            renderInput={(params) => <TextField {...params} />}
+          />
         </LocalizationProvider>
         <FormControl sx={{ m: 4 }}>
           <InputLabel htmlFor="outlined-adornment-amount">
@@ -91,12 +150,37 @@ function AddAsset() {
           />
         </FormControl>
         <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          value={branch}
+          onChange={handleBranch}
+          helperText="Please select your Branch"
+        >
+          {branches.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
           id="outlined-multiline-static"
           label="Asset Description"
           multiline
-          rows={4}
+          rows={6}
           defaultValue=""
         />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Seller's Name"
+          multiline
+          maxRows={4}
+          value={value}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="save_button">
+        <Button variant="outlined">Save</Button>
       </div>
     </Box>
   );
