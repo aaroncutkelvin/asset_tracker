8638e6a2303dbc57c54a8e2fd13e55f5aba9ea53
aaroncutkelvim <aaroncutkelvin@gmail.com>
2022-03-09 21:52:09 -0600

    add asset page

--- src/components/AddAsset.jsx
+++ src/components/AddAsset.jsx
@@ -1,10 +1,102 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import TimePicker from "@mui/lab/TimePicker";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+import MobileDatePicker from "@mui/lab/MobileDatePicker";
 
 function AddAsset() {
+  const [value, setValue] = React.useState("Controlled");
+  const [values, setValues] = React.useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
+
+  const handleChanges = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const [val, setVal] = React.useState(new Date("2022-03-10T21:11:54"));
+
+  const handleFix = (newVal) => {
+    setVal(newVal);
+  };
+
   return (
-    <div>
-      <h2>Add asset on this page</h2>
-    </div>
+    <Box
+      component="form"
+      justifyContent={"center"}
+      sx={{
+        "& .MuiTextField-root": { m: 4, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Asset Number"
+          multiline
+          maxRows={4}
+          value={value}
+          onChange={handleChange}
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Asset Name"
+          multiline
+          maxRows={4}
+          value={value}
+          onChange={handleChange}
+        />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker
+            label="Date desktop"
+            inputFormat="MM/dd/yyyy"
+            value={val}
+            onChange={handleFix}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <TimePicker
+            label="Time"
+            value={val}
+            onChange={handleFix}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <FormControl sx={{ m: 4 }}>
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            value={values.amount}
+            onChange={handleChanges("amount")}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+          />
+        </FormControl>
+        <TextField
+          id="outlined-multiline-static"
+          label="Asset Description"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+      </div>
+    </Box>
   );
 }
 
